18aeece63e9357a1a52dbae3e833b8d13799e459
gnairooze <gnairooze@gmail.com>
2016-06-05 15:24:54 +0200

    add youtube embed
    add youtube embed script and source

---
+++ youtube/sources/embed-source.js
@@ -0,0 +1,28 @@
+javascript:
+(
+	function()
+	{
+		name = "v"; 
+		url = window.location.href; 
+		name = name.replace(/[\[\]]/g, "\\$&"); 
+		prefix = "https://www.youtube.com/embed/"; 
+		code = null; 
+		
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url); 
+		
+		if (!results) 
+		{
+			code = null; 
+		} 
+		else if (!results[2]) 
+		{
+			code = '';
+		} 
+		else 
+		{ 
+			code = decodeURIComponent(results[2].replace(/\+/g, " ")); 
+		} 
+		
+		location.href = prefix + code
+	}
+)();
